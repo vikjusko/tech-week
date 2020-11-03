01df97b92805656b38d87f6ce01bbe3c2908bf1b
Victoria Jusko <vikjusko@gmail.com>
2020-11-03 11:39:20 +0000

    The account is initialized with a starting balance of zero, test passes

--- lib/account.rb
+++ lib/account.rb
@@ -1,2 +1,7 @@
-class Account 
-end 
+class Account
+	attr_reader :balance
+
+	def initialize
+		@balance = 0
+	end 
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,3 +1,9 @@
-require "account"
+require 'account'
 describe Account do
-end 
+  let(:account) { described_class.new }
+  context ' initializing the class' do
+    it 'Has an initial balance of zero' do
+      expect(account.balance).to eq 0
+    end
+  end
+end
