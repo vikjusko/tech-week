6b92e069cc9f860b5e5eefe9b8999455f7e65ea1
Victoria Jusko <vikjusko@gmail.com>
2020-11-03 12:06:46 +0000

    Added a print test method that passes

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,4 +1,8 @@
 require './lib/account.rb'
 
 class Statement 
+
+	def print
+		"date || credit || debit || balance\n03/11/2020 || 20.00 || || -20.00"
+	end 
 end 
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,3 +1,7 @@
 require "./lib/statement.rb"
 describe Statement do 
+subject(:statement) { described_class.new }
+	it "can print out the account transactions in a needed format" do
+  	expect(statement.print).to eq ("date || credit || debit || balance\n03/11/2020 || 20.00 || || -20.00")
+	end
 end 
