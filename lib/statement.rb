13cfe31a3ebe3d6875fd5c6aea3cf0ecd8869a70
Victoria Jusko <vikjusko@gmail.com>
2020-11-03 12:25:21 +0000

    Added tranactionlist method that stores transactions in an array

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,17 @@
 require './lib/account.rb'
 
 class Statement 
+	def initialize(account = Account.new)
+		@account = account
+		@transactions = []
+	end 
 
 	def print
 		"date || credit || debit || balance\n03/11/2020 || 20.00 || || -20.00"
 	end 
+
+	def transaction_list(log = @account.activity)
+  	@transactions = log
+	end
+
 end 
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,7 +1,16 @@
 require "./lib/statement.rb"
 describe Statement do 
-subject(:statement) { described_class.new }
+	let(:account) { Account.new }
+	subject(:statement) { described_class.new }
+
 	it "can print out the account transactions in a needed format" do
   	expect(statement.print).to eq ("date || credit || debit || balance\n03/11/2020 || 20.00 || || -20.00")
 	end
+
+	it "can store transactions from an assigned account" do
+		account.deposit(1500)
+  	expect(statement.transaction_list(account.activity)).to eq ["03/11/2020 || || 1500.00 || 1500.00"]
+	end
+	
+
 end 
